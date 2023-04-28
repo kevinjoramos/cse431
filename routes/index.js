87fe114c9074e8cd532d3074d3eba83ba6f2eb40
Kevin Ramos <kevin.jo.ramos@gmail.com>
2023-04-27 18:14:04 -0600

    Added mongodb connection, nd .env file

--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,6 @@ const {giveWifeFullName} = require("../controllers");
 const index = express.Router()
 
 index.get('/', giveWifeFullName)
+index.get('/contacts' )
 
 module.exports = index
--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express')
-const router = require("./routes");
+const router = require("./routes")
+const { run } = require("./db/connection")
+
 const app = express()
 const port = 8080
 
+run().catch(console.dir);
 
 app.use('/', router)
 
