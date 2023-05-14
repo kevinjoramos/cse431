8ac9a53a4710d23a19dcd79e3ea37782c192b92a
Kevin Ramos <kevin.jo.ramos@gmail.com>
2023-05-14 01:05:59 -0600

    troubleshooting doc failure.

--- utils/swagger.js
+++ utils/swagger.js
@@ -5,7 +5,7 @@ const doc = {
         title: 'My API',
         description: 'Description',
     },
-    host: 'localhost:3000',
+    host: 'https://cse341-b0h6.onrender.com',
     schemes: ['http'],
 };
 
