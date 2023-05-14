4ffadc08067fb6cbd2bc4ed8211b3680007fb014
Kevin Ramos <kevin.jo.ramos@gmail.com>
2023-05-13 23:35:05 -0600

    Added support for cors

--- server.js
+++ server.js
@@ -6,6 +6,20 @@ const app = express()
 const port = 8080
 
 openMongoConnection()
+
+// Add support for CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept'
+    );
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+    );
+    next();
+});
 app.use('/', router)
 
 app.listen(port, () => {
